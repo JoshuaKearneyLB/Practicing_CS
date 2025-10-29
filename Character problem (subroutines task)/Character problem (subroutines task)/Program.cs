82904c9f0d8e5ab1421b6427dccd8bcfa36d56cd
JoshuaKearneyLB <146336557+JoshuaKearneyLB@users.noreply.github.com>
2025-10-29 16:26:02 +0000

    Add files via upload

---
+++ Character problem (subroutines task)/Character problem (subroutines task)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Character_problem__subroutines_task_
+{
+    class Program
+    {
+        
+        // subroutines
+
+        static string digits()
+        {
+            return ("0123456789");
+        }
+
+        static string characters()
+        {
+            return ("abcdABCD@#!$");
+        }
+        
+        
+        // main to output both of the subroutines at once
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "The alpha numeric characters are: {0}{1}",digits(),characters());
+        }
+    }
+}
